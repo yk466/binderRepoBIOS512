0fc5225a06cdde5f4a104123984a98159e64882a
Victor M <vmdz@tutanota.com>
2025-09-14 17:35:09 -0400

    updated install.r

--- install.R
+++ install.R
@@ -2,5 +2,6 @@ install.packages("readr")
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("skimr", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
