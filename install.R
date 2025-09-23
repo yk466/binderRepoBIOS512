839c55c2523b50a53a32c7d239337816e62f19fc
Victor M <vmdz@tutanota.com>
2025-09-22 21:51:00 -0400

    added homework 5 files

--- install.R
+++ install.R
@@ -3,5 +3,6 @@ install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
+install.packages("Rtsne", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
